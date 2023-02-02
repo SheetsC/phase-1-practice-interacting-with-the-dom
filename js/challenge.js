6a0c888d89fc6f612eb37e33f8deadd4c07441e0
Matthew Longshore <matthewlongshore12@gmail.com>
2023-02-02 11:31:48 -0500

    like button working git add .

--- js/challenge.js
+++ js/challenge.js
@@ -1,16 +1,27 @@
 const timer = document.querySelector('#counter')
 const addition = document.querySelector('#plus')
 const subtract = document.querySelector('#minus')
+const like = document.querySelector('#heart')
+let likeClicks = 0
 addition.addEventListener('click', () => { 
     timer.textContent = parseInt(timer.textContent)+1
 })
 
 subtract.addEventListener('click', () => { 
-    timer.textContent = Math.abs(parseInt(timer.textContent)-1)
-    for( i=0; i>0; i++) {
-        return i;
+    if(timer.textContent <= 0) {
+        return 0
     }
+    timer.textContent = Math.abs(parseInt(timer.textContent)-1)
 })
+
+like.addEventListener('click', () => {
+    const likesList = document.querySelector('.likes')
+    const li = document.createElement('li')
+    likesList.append(li)
+    li.textContent = `${timer.textContent} has been liked ${likeClicks+=1} times`
+})
+
+
 
 
 
