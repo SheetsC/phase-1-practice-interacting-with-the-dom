1b1fc7241b5d86c99563647ce275de694e0e290c
Matthew Longshore <matthewlongshore12@gmail.com>
2023-02-02 11:03:25 -0500

    add and subtract functions

--- js/challenge.js
+++ js/challenge.js
@@ -1,3 +1,20 @@
-function addtimer(){
-    console.log("helo")
-}
+const timer = document.querySelector('#counter')
+const addition = document.querySelector('#plus')
+const subtract = document.querySelector('#minus')
+addition.addEventListener('click', () => { 
+    timer.textContent = parseInt(timer.textContent)+1
+})
+
+subtract.addEventListener('click', () => { 
+    timer.textContent = Math.abs(parseInt(timer.textContent)-1)
+    for( i=0; i>0; i++) {
+        return i;
+    }
+})
+
+
+
+
+
+
+
